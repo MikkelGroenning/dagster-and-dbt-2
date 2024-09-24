e39f11388a59e9f3eba8c3ce5b5ef91cbed00812
Mikkel Grønning <mikkel@groenning.net>
2024-09-24 13:17:53 +0200

    chore: Update deployment workflow to use Poetry for dependency management and set DBT_TARGET to 'prod'

--- dagster_university/project.py
+++ dagster_university/project.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from dagster_dbt import DbtProject
@@ -5,6 +6,8 @@ from dagster_dbt import DbtProject
 
 dbt_project = DbtProject(
     project_dir=Path(__file__).joinpath("..", "..", "analytics").resolve(),
+    target=os.getenv("DBT_TARGET")
 )
+
 
 dbt_project.prepare_if_dev()
