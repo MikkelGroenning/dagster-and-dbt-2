124ecd3899c44366cc65809f4b5f5e9abab7a259
Mikkel Grønning <mikkel@groenning.net>
2024-09-25 12:25:25 +0200

    chore: Update deployment workflow to enable fast deploys and use Python version 3.12

--- setup.py
+++ setup.py
@@ -1,24 +1,11 @@
 from setuptools import find_packages, setup
 
+with open('requirements.txt') as f:
+    required_packages = f.read().splitlines()
+
 setup(
     name="dagster_university",
     packages=find_packages(exclude=["dagster_university_tests"]),
-    install_requires=[
-        "dagster==1.7.*",
-        "dagster-cloud",
-        "dagster-duckdb",
-        "dagster-dbt",
-        "dbt-duckdb",
-        "geopandas",
-        "kaleido",
-        "pandas[parquet]",
-        "plotly",
-        "shapely",
-        "smart_open[s3]",
-        "s3fs",
-        "smart_open",
-        "boto3",
-        "pyarrow",
-    ],
+    install_requires=required_packages,
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
