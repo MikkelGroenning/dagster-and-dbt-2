b0d3ab0c4ca2d617d24358e62f75f3320188a205
Mikkel Grønning <mikkel@groenning.net>
2024-09-25 11:40:14 +0200

    chore: Update setup.py to use project-repo/requirements.txt for required packages

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import find_packages, setup
 with open('requirements.txt') as f:
     required_packages = f.read().splitlines()
 
-#required_packages = [req.split(';')[0] for req in required_packages]
-
 setup(
     name="dagster_university",
     packages=find_packages(exclude=["dagster_university_tests"]),
