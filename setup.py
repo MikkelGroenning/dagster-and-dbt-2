6b0362a807c57f39c56db289e3f51cf72d22fa3e
Mikkel Grønning <mikkel@groenning.net>
2024-09-25 12:15:03 +0200

    chore: Update setup.py to include required packages and their versions

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 from setuptools import find_packages, setup
 
-with open('requirements.txt') as f:
-    required_packages = f.read().splitlines()
-
 setup(
     name="dagster_university",
     packages=find_packages(exclude=["dagster_university_tests"]),
-    install_requires=required_packages,
+    install_requires=[
+        "dagster==1.7.*",
+        "dagster-cloud",
+        "dagster-duckdb",
+        "dagster-dbt",
+        "dbt-duckdb",
+        "geopandas",
+        "kaleido",
+        "pandas[parquet]",
+        "plotly",
+        "shapely",
+        "smart_open[s3]",
+        "s3fs",
+        "smart_open",
+        "boto3",
+        "pyarrow",
+    ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
